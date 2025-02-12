71eec297b44b720ae9fe3948479caa56797037b7
Tawakai <thezetman888@gmail.com>
2025-02-12 16:37:58 +0500

    hw2 changed

---
+++ src/main/java/kz/kaznu/amir/HW4/User.java
@@ -0,0 +1,21 @@
+package kz.kaznu.amir.HW4;
+
+public class User {
+    public String surname;
+    public String name;
+    public String secondName;
+    public int birthYear;
+    public String email;
+    public User(String surname, String name, String secondName, int birthYear, String email) {
+        this.surname = surname;
+        this.name = name;
+        this.secondName = secondName;
+        this.birthYear = birthYear;
+        this.email = email;
+    }
+    public void info() {
+        System.out.println("ФИО:" + surname + " " + name + " " + secondName);
+        System.out.println("Год рождения:" + birthYear);
+        System.out.println("e-mail:" + email);
+    }
+}
